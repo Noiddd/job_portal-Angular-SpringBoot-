95ec30920f0126306a0627de89214025a74b9823
Noiddd <dionang11@gmail.com>
2023-01-11 00:32:21 +0800

    register component with routes

--- jobportal-angular/src/app/app.module.ts
+++ jobportal-angular/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { JobseekerRegisterComponent } from './jobseeker-register/jobseeker-register.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'jobseeker/register', component: JobseekerRegisterComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, JobseekerRegisterComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ jobportal-angular/src/app/jobseeker-register/jobseeker-register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jobseeker-register',
+  templateUrl: './jobseeker-register.component.html',
+  styleUrls: ['./jobseeker-register.component.css']
+})
+export class JobseekerRegisterComponent {
+
+}
