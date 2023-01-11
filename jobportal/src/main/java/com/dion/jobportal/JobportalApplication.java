0d1c262320ff640303e66aaf697a885371384ca6
Noiddd <dionang11@gmail.com>
2023-01-11 23:18:05 +0800

    port

--- jobportal/src/main/java/com/dion/jobportal/JobportalApplication.java
+++ jobportal/src/main/java/com/dion/jobportal/JobportalApplication.java
@@ -1,7 +1,5 @@
 package com.dion.jobportal;
 
-import com.dion.jobportal.service.JobSeekerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
